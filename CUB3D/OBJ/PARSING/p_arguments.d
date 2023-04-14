efb917148835f666bcd97204ec60eea72a2307e7
kellycious <kellyhuynh@outlook.fr>
2023-04-14 21:42:05 +0200

    parsing arg OK

--- CUB3D/OBJ/PARSING/p_arguments.d
+++
@@ -1,6 +0,0 @@
-OBJ/PARSING/p_arguments.o: PARSING/p_arguments.c PARSING/../LIB/cub3d.h \
-  PARSING/../LIB/libft/libft.h
-
-PARSING/../LIB/cub3d.h:
-
-PARSING/../LIB/libft/libft.h:
--- CUB3D/OBJ/main.d
+++
@@ -1,5 +0,0 @@
-OBJ/main.o: main.c LIB/cub3d.h LIB/libft/libft.h
-
-LIB/cub3d.h:
-
-LIB/libft/libft.h:
