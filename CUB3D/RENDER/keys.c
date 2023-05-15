95ae87efab2e63ffdb2902eb1815605a935116b7
kellycious <kellyhuynh@outlook.fr>
2023-05-15 20:36:12 +0200

    missing player moves

--- CUB3D/RENDER/keys.c
+++ CUB3D/RENDER/keys.c
@@ -7,17 +7,25 @@ void	key_press(mlx_key_data_t key, void *param)
 	game = (t_map *) param;
 	if (key.key == MLX_KEY_W
 		&& (key.action == MLX_REPEAT || key.action == MLX_PRESS))
-		move_forward(data);
+		forward(data);
 	if (key.key == MLX_KEY_S
 		&& (key.action == MLX_REPEAT || key.action == MLX_PRESS))
-		move_backward(data);
+		backward(data);
 	if (key.key == MLX_KEY_A
 		&& (key.action == MLX_REPEAT || key.action == MLX_PRESS))
-		move_left(data);
+		left(data);
 	if (key.key == MLX_KEY_D
 		&& (key.action == MLX_REPEAT || key.action == MLX_PRESS))
-		move_right(data);
+		right(data);
 	if (key.key == MLX_KEY_ESCAPE
 		&& (key.action == MLX_REPEAT || key.action == MLX_PRESS))
 		return (exit_game(game));
 }
+
+void	forward(t_map *game)
+{
+	
+}
+void	backward(t_map *game)
+void	left(t_map *game)
+void	right(t_map *game)
