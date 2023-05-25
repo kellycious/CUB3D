a3f7f1a1345bb6ab4f7ffefc13aa161278124e60
khuynh <khuynh@student.42.fr>
2023-05-25 23:50:22 +0200

    working

--- CUB3D/OBJ/PARSING/utils_map.d
+++
@@ -1,8 +0,0 @@
-OBJ/PARSING/utils_map.o: PARSING/utils_map.c PARSING/../LIB/cub3d.h \
- PARSING/../LIB/libft/libft.h PARSING/../LIB/libft/get_next_line.h \
- PARSING/../LIB/libft/libft.h PARSING/../LIB/minilibx-linux/mlx.h
-PARSING/../LIB/cub3d.h:
-PARSING/../LIB/libft/libft.h:
-PARSING/../LIB/libft/get_next_line.h:
-PARSING/../LIB/libft/libft.h:
-PARSING/../LIB/minilibx-linux/mlx.h:
